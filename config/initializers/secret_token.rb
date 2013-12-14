e7769a9a83cd123b1c52f4d7a867b7e7b99dfb69
ali <k092017@nu.edu.pk>
2013-12-14 17:14:06 +0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gitcheckout::Application.config.secret_key_base = '22cb763f222bcfdc69f355a65d3331e09cf51b43fac629c8d2d25736a8089a5919286d6a98d6f7fed096d8f983388196466e150698700300405aba90fb61bc79'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gitcheckout::Application.config.session_store :cookie_store, key: '_gitcheckout_session'

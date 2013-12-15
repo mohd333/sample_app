260e064d64bef6ca5d73e410afb086721013b25f
ali <k092017@nu.edu.pk>
2013-12-15 09:12:45 +0500

    second push

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Gitcheckout::Application.routes.draw do
+
+  root :to => "examples#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
